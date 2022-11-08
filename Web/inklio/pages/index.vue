59048044a2c05e23b2da9394aded56a3407964b8
Jace Sangco <jsangco@gmail.com>
2022-11-07 20:28:43 -0800

    Updating index.vue to use fetch

--- Web/inklio/pages/index.vue
+++ Web/inklio/pages/index.vue
@@ -5,40 +5,40 @@ const todoStore = useTodoStore();
 
 const runtimeConfig = useRuntimeConfig();
 const baseurl = runtimeConfig.public.baseUrl;
-// const ext = await todoStore.getExt();
-// const auth = await useFetch(`${runtimeConfig.public.baseUrl}/auth/`);
-// const authX = await useFetchX('/auth/');
-// const asks = await useFetchX('api/v1/asks/');
+const ext = await todoStore.getExt();
+const auth = await useFetch(`${runtimeConfig.public.baseUrl}/auth/`);
+const authX = await useFetchX('/auth/');
+const asks = await useFetchX('api/v1/asks/');
 
-// const login = await useFetchX(
-//       'https://inklio.azurewebsites.net/auth/accounts/login', {
-//       method: 'POST',
-//       body: {
-//         username: 'jace',
-//         password: 'Aoeuaoeu1'
-//         },
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//     });
+const login = await useFetchX(
+      'https://inklio.azurewebsites.net/auth/accounts/login', {
+      method: 'POST',
+      body: {
+        username: 'jace',
+        password: 'Aoeuaoeu1'
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    });
 
-// const authBasic = ref();
-// onBeforeMount( async () => {
-//     console.log('onBeforemounted');
-//     authBasic.value = await todoStore.getAuthBasic();
-// })
+const authBasic = ref();
+onBeforeMount( async () => {
+    console.log('onBeforemounted');
+    authBasic.value = await todoStore.getAuthBasic();
+})
 </script>
 
 <template>
     <div>
         <h1>index</h1>
         <p>baseurl: {{baseurl}}</p>
-        <!-- <p><b>javascript-fetch store GET {{baseurl}}/auth:</b> {{authBasic}}</p>
+        <p><b>javascript-fetch store GET {{baseurl}}/auth:</b> {{authBasic}}</p>
         <p><b>ext api $fetch:</b> {{ ext }}</p>
         <p><b>useFetch {{baseurl}}/auth:</b> {{auth}}</p>
         <p><b>useFetchX {{baseurl}}/auth:</b> {{authX}}</p>
         <p><b>useFetchX {{baseurl}}/api/v1/asks:</b> {{asks}}</p>
-        <p><b>useFetchX {{baseurl}}/auth/accounts/login:</b> {{login}}</p> -->
+        <p><b>useFetchX {{baseurl}}/auth/accounts/login:</b> {{login}}</p>
     </div>
 </template>
 
