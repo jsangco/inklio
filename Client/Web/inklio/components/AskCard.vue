53e2e1bb0ddaffa1345a0914bffc1047ce211caa
Jace Sangco <jsangco@gmail.com>
2023-09-19 20:43:36 -0700

    Adding some basic style to the front page ask cards

--- Client/Web/inklio/components/AskCard.vue
+++ Client/Web/inklio/components/AskCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <div>Ask</div>
-    <span>{{ ask.id }}. {{ ask.title }}</span> - <span>{{ ask.body }}, {{ ask.createdBy }}</span>
+  <div class="askcard askcard-wrapper">
+    <h1>{{ ask.title }}</h1>
+    <p>{{ ask.body }}</p>
     <AskCardDelivery v-if="ask.deliveries.length > 0" :delivery="ask.deliveries[0]"/>
   </div>
 </template>
@@ -13,3 +13,36 @@ const props = defineProps<{
 }>();
 
 </script>
+
+<style>
+.askcard h1 {
+  font-size: large;
+  margin: 4px;
+}
+
+.askcard p {
+  font-size: small;
+  margin: 4px;
+}
+
+.askcard img {
+  object-fit: cover;
+  max-width: 300px;
+  max-height: 300px;
+}
+
+.askcard-wrapper{
+  background-color: rgb(46, 46, 46);
+  border: solid rgb(0, 0, 0) 5px;
+  border-radius: 10px;
+  padding: 5px;
+  margin-left: 40px;
+  margin-right: 40px;
+  width: 440px;
+}
+
+.askcard-wrapper:hover {
+  background-color: rgb(57, 57, 57);
+
+}
+</style>
--- Client/Web/inklio/components/AskCardDelivery.vue
+++ Client/Web/inklio/components/AskCardDelivery.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <div>Delivery</div>
-    <div v-if="delivery.title">{{ delivery.title }}</div>
+  <div class="askcarddelivery askcarddelivery-wrapper">
     <img :src="delivery.images[0].url"/>
-    <div v-if="delivery.body">{{ delivery.body }}</div>
+    <div class="askcarddelivery-overlay">
+      <h1 v-if="delivery.title">{{ delivery.title }}</h1>
+      <p v-if="delivery.body">{{ delivery.body }}</p>
+    </div>
   </div>
 </template>
 
@@ -15,3 +16,41 @@ const props = defineProps<{
 }>();
 
 </script>
+
+<style>
+.askcarddelivery-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .6);
+  display: flex;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.askcarddelivery h1{
+  text-align: left;
+}
+
+.askcarddelivery p{
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+}
+
+.askcarddelivery-wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-color: grey;
+  border: none rgb(0, 0, 0) 2px;
+  border-radius: 10px;
+  padding: 5px;
+  margin-left: 10px;
+  width: 400px;
+  overflow: hidden;
+}
+</style>
